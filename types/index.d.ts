080089e4d07d656eaf26a41d302a9ab6b6a14e85
syphzur <krzysztof.bol@wp.pl>
2022-08-01 21:46:47 +0200

    feat: typescript types definition

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+import type { RecurseDefaults } from 'cypress-recurse';
+
+declare namespace Cypress {
+  interface Chainable<Subject = any> {
+    /**
+     * @param name The name of the snapshots that will be generated
+     * @param testThreshold @default 0 A number between 0 and 1 that represents the allowed percentage of pixels that can be different between the two snapshots
+     * @param retryOptions @default { limit: 1 } @see {@link RecurseDefaults}
+     * @example cy.compareSnapshot('empty-canvas', 0.1)
+     */
+    compareSnapshot(
+      name: string,
+      testThreshold?: number,
+      retryOptions?: Partial<typeof RecurseDefaults>
+    ): Chainable<Element>
+  }
+}
