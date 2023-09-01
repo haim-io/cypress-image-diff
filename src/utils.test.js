1ef12340c1eeabe68c6b89050743498f5b303d21
KienHT <71423573+kien-ht@users.noreply.github.com>
2023-09-02 00:30:35 +0700

    feat: add unit tests for new util functions

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,5 +1,5 @@
-import { existsSync, mkdirSync, emptyDirSync, readdirSync, moveSync, copySync } from 'fs-extra'
-import { createDir, cleanDir, renameAndMoveFile, renameAndCopyFile } from './utils'
+import { existsSync, mkdirSync, emptyDirSync, readdirSync, moveSync, copySync, writeFile } from 'fs-extra'
+import { createDir, cleanDir, renameAndMoveFile, renameAndCopyFile, getRelativePathFromCwd, getCleanDate, writeFileIncrement } from './utils'
 
 jest.mock('fs-extra', () => ({
   ...jest.requireActual('fs-extra'),
@@ -9,6 +9,7 @@ jest.mock('fs-extra', () => ({
   readdirSync: jest.fn(),
   moveSync: jest.fn(),
   copySync: jest.fn(),
+  writeFile: jest.fn(),
 }))
 
 describe('Utils', () => {
@@ -70,6 +71,64 @@ describe('Utils', () => {
       renameAndCopyFile(sampleFiles[0], sampleFiles[1])
       expect(copySync).toHaveBeenCalledTimes(1)
       expect(copySync).toBeCalledWith(sampleFiles[0], sampleFiles[1], {"overwrite": true})
+    })
+  })
+
+  describe('Get relative path from the current working directory', () => {
+    const processCwd = jest.spyOn(process, 'cwd')
+    const fakeCwd = 'User/my-project/'
+
+    beforeEach(() => {
+      processCwd.mockReturnValue(fakeCwd)
+    })
+
+    it('should return empty string when path doesn\'t exist', () => {
+      existsSync.mockReturnValue(false)
+      
+      const relativePath = getRelativePathFromCwd('User/my-project/cypress/screenshot.png')
+      expect(relativePath).toBe('')
+    })
+  
+    it('should return a relative path when given path exists', () => {
+      existsSync.mockReturnValue(true)
+
+      const relativePath = getRelativePathFromCwd('User/my-project/cypress/screenshot.png')
+      expect(relativePath).toBe('cypress/screenshot.png')
+    })
+  })
+
+  describe('Get clean date string', () => {
+    afterEach(() => {
+        jest.restoreAllMocks()
+    })
+
+    it('should return a clean date string', () => {
+      const fakeDate = '01/09/2023, 23:22:48'
+      jest.spyOn(Date.prototype, 'toLocaleString').mockReturnValue(fakeDate)
+
+      expect(getCleanDate()).toBe('01-09-2023_232248')
+    })
+  })
+
+  describe('Write incremented filename', () => {
+    const filename = 'User/my-project/report.json'
+    const filenameIncremented = 'User/my-project/report_2.json'
+    const fakeData = '{\n  "name": "test"\n}'
+
+    it('should create a new file with given name when no filename found', () => {
+      existsSync.mockReturnValue(false)
+
+      writeFileIncrement(filename, fakeData)
+      expect(writeFile).toHaveBeenCalledTimes(1)
+      expect(writeFile).toBeCalledWith(filename, fakeData)
+    })
+
+    it('should increment filename when it already exists', () => {
+      existsSync.mockReturnValueOnce(true).mockReturnValueOnce(false)
+
+      writeFileIncrement(filename, fakeData)
+      expect(writeFile).toHaveBeenCalledTimes(1)
+      expect(writeFile).toBeCalledWith(filenameIncremented, fakeData)
     })
   })
 })
