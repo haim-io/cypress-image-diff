30b970831d3a7bc39286255b33caf1fb40dfebb3
KienHT <71423573+kien-ht@users.noreply.github.com>
2023-11-14 09:40:57 +0000

    feat: make screenshots and report folder configurable

--- src/config.test.js
+++ src/config.test.js
@@ -2,6 +2,20 @@ import { Paths } from './config'
 
 describe('Path config', () => {
   const processCwd = jest.spyOn(process, 'cwd')
+  const userConfig = {
+    ROOT_DIR: '',
+    REPORT_DIR: 'cypress-image-diff-html-report',
+    SCREENSHOTS_DIR: 'cypress-image-diff-screenshots',
+    FAILURE_THRESHOLD: 0,
+    RETRY_OPTIONS: {},
+    FAIL_ON_MISSING_BASELINE: false,
+    COMPARISON_OPTIONS: { threshold: 0.1 },
+    JSON_REPORT: {
+      FILENAME: '',
+      OVERWRITE: true,
+    },
+    CYPRESS_SCREENSHOT_OPTIONS: {},
+  }
 
   afterEach(() => {
     jest.clearAllMocks()
@@ -10,7 +24,7 @@ describe('Path config', () => {
   it('should return default folder structure', () => {
     processCwd.mockReturnValue('User/my-project/')
 
-    const paths = new Paths()
+    const paths = new Paths(userConfig)
 
     expect(paths.image.baseline('test')).toBe(
       'User/my-project/cypress-image-diff-screenshots/baseline/test.png'
@@ -38,7 +52,7 @@ describe('Path config', () => {
   it('should return custom folder structure', () => {
     processCwd.mockReturnValue('User/my-project/')
 
-    const paths = new Paths()
+    const paths = new Paths(userConfig)
     paths.rootDir = 'visual-test/custom-folder-name'
 
     expect(paths.image.baseline('test')).toBe(
