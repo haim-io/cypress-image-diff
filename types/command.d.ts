5ed9e336f552e03295e507b2d3006e06ad5a804b
KienHT <71423573+kien-ht@users.noreply.github.com>
2023-12-17 09:53:47 +0000

    fix(typings): compareSnapshot typings and add missing hideElement command

--- types/command.d.ts
+++ types/command.d.ts
@@ -1,9 +1,18 @@
 
 import type { CompareSnapshotOptions } from './options'
 
-declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    compareSnapshot(options: string | CompareSnapshotOptions): Chainable<Element>
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject = any> {
+      /**
+       * custom command to take screenshots and compare them with their baseline images
+       */
+      compareSnapshot(options: string | CompareSnapshotOptions): Chainable<Element>
+      /**
+       * custom command to hide/unhide elements
+       */
+      hideElement(hide?: boolean): void
+    }
   }
 }
 
