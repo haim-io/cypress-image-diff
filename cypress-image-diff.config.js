200345b1e3a9e27ae1cc5dcc3f864c6883030228
ash14 <ash14@users.noreply.github.com>
2026-03-03 15:58:12 +0800

    feat: Improve failure messages

--- cypress-image-diff.config.js
+++ cypress-image-diff.config.js
@@ -1,7 +1,8 @@
 const config = {
   ROOT_DIR: '',
   // disableTimersAndAnimations: false is necessary for retry-spec test
-  CYPRESS_SCREENSHOT_OPTIONS: { disableTimersAndAnimations: false }
+  CYPRESS_SCREENSHOT_OPTIONS: { disableTimersAndAnimations: false },
+  FAIL_ON_MISSING_BASELINE: Boolean(process.env.CYPRESS_FAIL_ON_MISSING_BASELINE),
 };
 
 module.exports = config;
